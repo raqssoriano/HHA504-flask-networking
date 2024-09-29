d54a977cc3be55f4f0d8954f0549a4ce84ea082e
raqssoriano <mrrsoriano@yahoo.com>
2024-09-29 18:04:26 -0400

    networking

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify
 
-## Create a Flask app
+### Create a Flask app
 app = Flask(__name__)
 
-## Define a route
+### Define a route
 @app.route('/', methods=['GET'])
 def hello_world():
     return render_template('index.html')
 
-## run on port 5000
+### Run on port 5000
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
