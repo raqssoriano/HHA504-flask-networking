a76717ed49ba4e342b7986c631b382e17e220edc
raqssoriano <mrrsoriano@yahoo.com>
2024-09-29 19:09:29 -0400

    to push some minor changes

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 def life_is_beautiful():
     return "Life is beautiful!"
 
-### Run on port 5000
+### Run on port 5005
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5007)
+    
